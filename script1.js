88e94d0c6eb790f3e87c748ef02ded9e0ae5fed1
sashagebert <akataev1819@gmail.com>
2019-09-22 21:50:35 +0100

    Cleaned up code + changed design

--- script1.js
+++ script1.js
@@ -1,6 +1,28 @@
+let productImage = document.getElementById('productImage');
+let goldBlack = document.getElementById('goldBlack');
+let blueWhite = document.getElementById('blueWhite');
+let colour = '';
+
+animationend = (id, func) => {
+	id.addEventListener('animationend', func);
+};
+
+animationend(goldBlack, endFunctionProduct);
+animationend(blueWhite, endFunctionProduct);
+animationend(productImage, endFunctionProduct);
+
 function onLoadFunc() {
-  document.getElementById("productImage").classList.add("fadeInAnimation");
+	colour = 'black';
+	productImage.classList.add('fadeInAnimation');
 }
+
+goldBlack.onclick = () => {
+	colour = 'black';
+};
+
+endFunctionProduct = () => {
+	productImage.classList.remove('fadeInAnimation');
+};
 
 //this animation works remove the class when it ends plus add animation to other elements
 //https://stackoverflow.com/questions/11660710/css-transition-fade-in
