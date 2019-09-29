b11f47a4fdf24081d593ea42cc84c61ff0507127
sashagebert <43831869+sashagebert@users.noreply.github.com>
2019-09-29 02:32:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
-<?php 
+<?php
 $servername = "eu-cdbr-west-02.cleardb.net";
 $username = "bd7f314c063cdf";
 $password = "bfeb2057";
 
 // Create connection
-$link = mysqli_connect( $servername, $username, $password )
-or die( "Unable to Connect" );
+$conn = new mysqli($servername, $username, $password);
+
 // Check connection
-
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+      
 $sql = "SELECT * FROM products WHERE id = 'hy1'";
-$result = mysqli_query($link, $sql);
+$result = $conn->query($sql);
 
-while($row = mysqli_fetch_array( $result, MYSQLI_ASSOC) {
-     echo "<br> id: ". $row["id"]. ", name: ". $row["name"]. ", section: " . $row["section"] . ", isSize: " . $row["isSizeNum"] . "colour: " . $row["colourName"] . "<br>";
-  }
-
-mysqli_close($link);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["id"]. ", name: ". $row["name"]. ", section: " . $row["section"] . ", isSize: " . $row["isSizeNum"] . "colour: " . $row["colourName"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+      
 
 ?>
