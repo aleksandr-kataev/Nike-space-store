4632faf2b792f9b343dfc5690e37a9b1cd5d2cba
sashagebert <43831869+sashagebert@users.noreply.github.com>
2019-09-29 02:12:23 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+$servername = "eu-cdbr-west-02.cleardb.net";
+$username = " bd7f314c063cdf";
+$password = " bfeb2057";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT * FROM products WHERE id = 'hy1'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["id"]. ", name: ". $row["name"]. ", section: " . $row["section"] . ", isSize: " . $row["isSizeNum"] . "colour: " . $row["colourName"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+
+?>
