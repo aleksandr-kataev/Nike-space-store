46d5d672912f4f89f870692e5d3e26c6c2035429
sashagebert <43831869+sashagebert@users.noreply.github.com>
2019-10-03 15:08:39 +0100

    Landing page for desktop

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+		<title>Airmax 95</title>
+		<link rel="stylesheet" type="text/css" href="landing.style.css" />
+		<link rel="stylesheet" href="https://use.typekit.net/chz6iqm.css" />
+		<link
+			href="https://fonts.googleapis.com/css?family=Open+Sans:300i,400&display=swap"
+			rel="stylesheet"
+		/>
+	</head>
+
+	<body>
+		<div id="cntCanvas">
+			<div class="cntMain">
+				<div id="cntLogo">
+					<p id="nike">Nike</p>
+					<p id="space">Space</p>
+					<img src="images/Logo.svg" alt="Logo" />
+					<br />
+					<button id="instagram"></button>
+					<button id="twitter"></button>
+					<button id="youtube"></button>
+				</div>
+				<div id="cntDelivery"><p>delivery</p></div>
+				<div id="cntNavBar">
+					<button class="navButton" id="men">
+						<span class="strike">MEN</span>
+					</button>
+					<button class="navButton" id="women">
+						<span class="strike">WOMEN</span>
+					</button>
+					<button class="navButton" id="kids">
+						<span class="strike">KIDS</span>
+					</button>
+					<button id="shoppingCart"></button>
+				</div>
+				<div id="cntTitle">
+					<div id="heading">VAPOURMAX</div>
+					<div id="subheading">AVALIABLE NOW</div>
+				</div>
+				<div id="cntCircleTopRight">
+					<img src="./images/CircleTopRight.png" alt="Background Circle" />
+				</div>
+				<div id="cntDescription" class="elementCenter">
+					Be bouncy and buoyant in the Nike Air VaporMax Flyknit 3. Inspired by
+					high fashion, the upper features flowing lines of breathable,
+					stretchable Flyknit construction. Revolutionary VaporMax Air
+					technology keeps a spring in your step with toe-to-heel cushioning.
+				</div>
+				<div id="cntFeatures">
+					<ul>
+						<li>
+							- VaporMax Air technology provides soft, lightweight
+							responsiveness.
+						</li>
+						<li>
+							- Flyknit upper seamlessly integrates breathability, stretch and
+							support.
+						</li>
+						<li>
+							- Textured upper takes inspiration from the flowing, linear lines
+							of couture fashion.
+						</li>
+					</ul>
+				</div>
+				<div id="cntImage" class="elementCenter">
+					<img src="./images/vp1.png" alt="Background Circle" />
+				</div>
+				<div id="cntButton">
+					<button id="button" class="elementCenter">
+						SHOP NOW
+					</button>
+				</div>
+				<div id="cntCircle">
+					<img id="Circle" src="./images/Circle.png" alt="Background Circle" />
+				</div>
+				<div id="cntCircleBlur">
+					<img src="./images/CircleBlur.png" alt="Background Circle" />
+				</div>
+				<div id="cntSphere">
+					<img src="./images/Sphere.png" id="Sphere" alt="Background Circle" />
+				</div>
+			</div>
+			<canvas></canvas>
+		</div>
+	</body>
+	<script type="text/javascript" src="./landing.js"></script>
+</html>
---
+++ landing.js
@@ -0,0 +1,169 @@
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let c = canvas.getContext('2d');
+let particleArr;
+
+var mouse = {
+	x: undefined,
+	y: undefined,
+};
+
+window.addEventListener('mousemove', (event) => {
+	mouse.x = event.x;
+	mouse.y = event.y;
+});
+
+function Particle(x, y, radius, color) {
+	this.x = x;
+	this.y = y;
+	this.velocity = {
+		x: (Math.random() - 0.5) * 5,
+		y: (Math.random() - 0.5) * 5,
+	};
+	this.radius = radius;
+	this.style = color;
+	this.mass = 10000;
+
+	this.draw = () => {
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+		c.strokeStyle = this.style;
+		c.stroke();
+		c.fillStyle = this.style;
+		c.fill();
+		c.closePath();
+	};
+
+	this.update = (particleArr) => {
+		this.draw();
+
+		for (let i = 0; i < particleArr.length; i++) {
+			if (this === particleArr[i]) continue;
+
+			if (
+				distance(this.x, this.y, particleArr[i].x, particleArr[i].y) -
+					this.radius * 2 <
+				0
+			) {
+				resolveCollision(this, particleArr[i]);
+			}
+		}
+
+		if (this.x - this.radius <= 0 || this.x + this.radius >= innerWidth) {
+			this.velocity.x = -this.velocity.x;
+		}
+
+		if (this.y - this.radius <= 0 || this.y + this.radius >= innerHeight) {
+			this.velocity.y = -this.velocity.y;
+		}
+
+		if (distance(mouse.x, mouse.y, this.x, this.y) < 100) {
+			this.style = 'black';
+			this.radius = 3;
+		} else {
+			this.style = color;
+			this.radius = radius;
+		}
+
+		this.x += this.velocity.x;
+		this.y += this.velocity.y;
+	};
+}
+rotate = (velocity, angle) => {
+	const rotatedVelocities = {
+		x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+		y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle),
+	};
+
+	return rotatedVelocities;
+};
+
+resolveCollision = (particle, otherParticle) => {
+	const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+	const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+
+	const xDist = otherParticle.x - particle.x;
+	const yDist = otherParticle.y - particle.y;
+
+	// Prevent accidental overlap of particles
+	if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+		// Grab angle between the two colliding particles
+		const angle = -Math.atan2(
+			otherParticle.y - particle.y,
+			otherParticle.x - particle.x,
+		);
+
+		// Store mass in var for better readability in collision equation
+		const m1 = particle.mass;
+		const m2 = otherParticle.mass;
+
+		// Velocity before equation
+		const u1 = rotate(particle.velocity, angle);
+		const u2 = rotate(otherParticle.velocity, angle);
+
+		// Velocity after 1d collision equation
+		const v1 = {
+			x: (u1.x * (m1 - m2)) / (m1 + m2) + (u2.x * 2 * m2) / (m1 + m2),
+			y: u1.y,
+		};
+		const v2 = {
+			x: (u2.x * (m1 - m2)) / (m1 + m2) + (u1.x * 2 * m2) / (m1 + m2),
+			y: u2.y,
+		};
+
+		// Final velocity after rotating axis back to original location
+		const vFinal1 = rotate(v1, -angle);
+		const vFinal2 = rotate(v2, -angle);
+
+		// Swap particle velocities for realistic bounce effect
+		particle.velocity.x = vFinal1.x;
+		particle.velocity.y = vFinal1.y;
+
+		otherParticle.velocity.x = vFinal2.x;
+		otherParticle.velocity.y = vFinal2.y;
+	}
+};
+
+randomIntRange = (min, max) => {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+};
+distance = (x1, y1, x2, y2) => {
+	const xDist = x2 - x1;
+	const yDist = y2 - y1;
+	return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+};
+
+init = () => {
+	particleArr = [];
+	const radius = 1;
+	for (let i = 0; i < 700; i++) {
+		let x = randomIntRange(radius, canvas.width - radius);
+		let y = randomIntRange(radius, canvas.height - radius);
+		const color = 'rgba(255, 255, 255, 0.1)';
+		if (i !== 0) {
+			for (let j = 0; j < particleArr.length; j++) {
+				if (
+					distance(x, y, particleArr[j].x, particleArr[j].y) - radius * 2 <
+					0
+				) {
+					x = randomIntRange(radius, canvas.width - radius);
+					y = randomIntRange(radius, canvas.height - radius);
+					j = -1;
+				}
+			}
+		}
+		particleArr.push(new Particle(x, y, radius, color));
+	}
+};
+
+animate = () => {
+	requestAnimationFrame(animate);
+	c.clearRect(0, 0, innerWidth, innerHeight);
+	particleArr.forEach((particle) => {
+		particle.update(particleArr);
+	});
+};
+
+init();
+animate();
